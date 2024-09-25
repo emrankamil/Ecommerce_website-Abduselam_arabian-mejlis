fc5c68d374e8765bae1397d63bab59530bd4caf3
emran <emranhi001@gmail.com>
2024-09-26 01:06:30 +0300

    completed landing page

--- server/utils/image_processing.go
+++ server/utils/image_processing.go
@@ -1,12 +1,11 @@
 package utils
 
 import (
-    "fmt"
-    "os"
-    "strings"
+	"fmt"
+	"os"
+	"strings"
 
-    "github.com/google/uuid"
-    "github.com/h2non/bimg"
+	"github.com/google/uuid"
 )
 
 // A new folder is created at the root of the project.
@@ -23,19 +22,27 @@ func CreateFolder(dirname string) error {
 
 // The mime type of the image is changed, it is compressed and then saved in the specified folder.
 func ImageProcessing(buffer []byte, quality int, dirname string) (string, error) {
-    filename := strings.Replace(uuid.New().String(), "-", "", -1) + ".webp"
+    // filename := strings.Replace(uuid.New().String(), "-", "", -1) + ".webp"
 
-    converted, err := bimg.NewImage(buffer).Convert(bimg.WEBP)
-    if err != nil {
-        return filename, err
-    }
+    // converted, err := bimg.NewImage(buffer).Convert(bimg.WEBP)
+    // if err != nil {
+    //     return filename, err
+    // }
 
-    processed, err := bimg.NewImage(converted).Process(bimg.Options{Quality: quality})
-    if err != nil {
-        return filename, err
-    }
+    // processed, err := bimg.NewImage(converted).Process(bimg.Options{Quality: quality})
+    // if err != nil {
+    //     return filename, err
+    // }
 
-    writeError := bimg.Write(fmt.Sprintf("./"+dirname+"/%s", filename), processed)
+    // writeError := bimg.Write(fmt.Sprintf("./"+dirname+"/%s", filename), processed)
+    // if writeError != nil {
+    //     return filename, writeError
+    // }
+
+    // return filename, nil
+    filename := strings.Replace(uuid.New().String(), "-", "", -1) + ".jpg" // Adjust the extension as needed
+
+    writeError := os.WriteFile(fmt.Sprintf("./"+dirname+"/%s", filename), buffer, 0644)
     if writeError != nil {
         return filename, writeError
     }
